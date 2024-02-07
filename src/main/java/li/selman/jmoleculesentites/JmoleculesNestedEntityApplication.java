f7720bbf3e5ba15950a44194280d6301a9b931dc
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-02-07 21:24:49 +0100

    Init Repo

---
+++ src/main/java/li/selman/jmoleculesentites/JmoleculesNestedEntityApplication.java
@@ -0,0 +1,13 @@
+package li.selman.jmoleculesentites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmoleculesNestedEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmoleculesNestedEntityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/li/selman/jmoleculesentites/JmoleculesNestedEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package li.selman.jmoleculesentites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmoleculesNestedEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
